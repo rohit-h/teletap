323c13342754910db77750bbf49dd500e61f2ce1
vasuwrath <vasudevan.m27@gmail.com>
2015-02-11 00:15:28 +0530

    Packaged the library and added setup script.
    - Refined directory structure to package the library.
    - Added setup script to install the package.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Teletap',
+      version='1.0',
+      description='Telegram wrapper library for telegram-cli',
+      author='Rohit R.xH',
+      url='https://github.com/rohit-h/teletap',
+      license = 'GPL',
+      packages=['teletap'],
+     )
